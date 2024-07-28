3aaf08af6a28c614e24db57d1546d88e4d2bdd1c
mohamedkeliwa <mhmdalknj496@gmail.com>
2024-07-28 21:04:11 +0300

    chore: Add name input and submit functionality to Player component

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,22 @@
+import React, { useState, useRef } from 'react';
+
 export default function Player() {
+  const [name, setName] = useState(null);
+  const [submitted, setSubmitted] = useState(false);
+
+  function handleName(event) {
+    setSubmitted(false);
+    setName(event.target.value);
+  }
+  function hadleSubmit() {
+    setSubmitted(true);
+  }
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome { submitted ? name : 'unknown entity' } </h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" value={ name } onChange={ handleName } />
+        <button onClick={ hadleSubmit } >Set Name</button>
       </p>
     </section>
   );
